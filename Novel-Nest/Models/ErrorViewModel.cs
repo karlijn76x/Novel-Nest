10a2e98c036f0c0ccca932daa6ac5f10804494ca
A.M.K. van den Herik <karlijnvandenherik@gmail.com>
2024-03-22 10:48:16 +0100

    Add project files.

---
+++ Novel-Nest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Novel_Nest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
